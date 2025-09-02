f05d5ccb9e42a2e1b7b37107b1b03f51bcb50598
Juan C <jcdiaz.9291@unicesmag.edu.co>
2025-09-02 15:33:25 -0500

    Table users

---
+++ config/database_backup/table_users.sql
@@ -0,0 +1,33 @@
+--Create tabke users
+
+CREATE TABLE users(
+        id BIGSERIAL PRIMARY KEY,
+        firstname VARCHAR(30) NOT NULL
+        lastname VARCHAR(30) NOT NULL
+        mobile_number VARCHAR(20) NOT NULL
+        ide_number VARCHAR(15) NULL UNIQUE,
+        address TEXT NULL,
+        birthday DATE NULL,
+        email VARCHAR(200) NOT NULL UNIQUE,
+        password TEXT NOT NULL,
+        status BOOLEAN NOT NULL DEFAULT TRUE,
+        created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+        update_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+        deleted_at TIMESTAMPTZ NULL
+);
+
+-- Insert into table users
+INSERT INTO users(
+              firstname,
+              lastname,
+              mobile_number,
+              email,
+              password 
+)
+ VALUES (
+         'Gregorio',
+         'Betraña',
+         '3001281282',
+         'gregoriobt@gmail.com',
+         '2112',
+);
