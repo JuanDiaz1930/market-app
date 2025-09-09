f775826309877fce5bd68ca34b65f72c5f6dcd37
Juan C <juanitoperez2025@gmail.com>
2025-09-09 15:31:47 -0500

    Request signup

--- src/signup.php
+++ src/signup.php
@@ -1,1 +1,31 @@
-database connect
+<?php
+
+   //Step1. Get database access
+   require('../config/database.php');
+
+
+   //Step2. Get form-date
+   $f_name= $_POST['fname'];   //Los campos que van aqui en el corchete deben
+   $l_name= $_POST['lname']; //ser iguales al alias que vienen del fronden
+   $m_number=$_POST['mnumber'];
+   $id_number=$_POST['idnumber'];
+   $e_mail=$_POST['email'];
+   $p_wd=$_POST['passwd'];
+
+
+   //Step3.Create query to INSERT INTO.
+   $query ="INSERT INTO users (firstname, lastname, mobile_number, ide_number, email, password)
+   Values ('$f_name', '$l_name', '$m_number', '$id_number', '$e_mail', '$p_wd')";
+
+
+   //Step4. Execute query
+
+   $res = pg_query($conn, $query);
+
+   //Step5. Validate result
+   if($res){
+    echo "User has been created successfully !!! :D";
+   }else{
+    echo "Something wrong! :p";
+   }
+?>
