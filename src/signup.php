f0ac7e7593fb5073b9e8d08d99a3e94a16df3420
Juan C <juanitoperez2025@gmail.com>
2025-09-11 10:51:14 -0500

    Encryp password

--- src/signup.php
+++ src/signup.php
@@ -12,10 +12,12 @@
    $e_mail=$_POST['email'];
    $p_wd=$_POST['passwd'];
 
+   $enc_pass = password_hash($p_wd, PASSWORD_DEFAULT);
+
 
    //Step3.Create query to INSERT INTO.
    $query ="INSERT INTO users (firstname, lastname, mobile_number, ide_number, email, password)
-   Values ('$f_name', '$l_name', '$m_number', '$id_number', '$e_mail', '$p_wd')";
+   Values ('$f_name', '$l_name', '$m_number', '$id_number', '$e_mail', '$enc_pass')";
 
 
    //Step4. Execute query
