478fece0c0fc1037091d4fd90d580c3d154ee20d
Juan C <juanitoperez2025@gmail.com>
2025-09-16 15:35:23 -0500

    IntegracionRegisterUser front-end

--- src/signup.php
+++ src/signup.php
@@ -14,8 +14,22 @@
 
    $enc_pass = password_hash($p_wd, PASSWORD_DEFAULT);
 
-
-   //Step3.Create query to INSERT INTO.
+   $check_email="
+       select 
+       u.email
+       from
+       user.u
+       where
+       email='$e_mail' or ide_number = '$id_number'
+       LIMIT 1
+   
+   ";
+   $res_check = pg_query($conn, $check_email);
+   if(pg_num_rows($res_check) > 0){
+      echo "<script>alert('User already exist !!')</script>";
+    header('refresh_0;url=signup.html');
+   }else{
+         //Step3.Create query to INSERT INTO.
    $query ="INSERT INTO users (firstname, lastname, mobile_number, ide_number, email, password)
    Values ('$f_name', '$l_name', '$m_number', '$id_number', '$e_mail', '$enc_pass')";
 
@@ -26,8 +40,11 @@
 
    //Step5. Validate result
    if($res){
-    echo "User has been created successfully !!! :D";
+    //echo "User has been created successfully !!! :D";
+    echo "<script>alert('Success !!! Go to login')</script>";
+    header('refresh_0;url=signin.html');
    }else{
     echo "Something wrong! :p";
+   }
    }
 ?>
