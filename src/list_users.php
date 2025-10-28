780f9855496bbd70610bef71f542e3d36d110ad5
Juan C <juanitoperez2025@gmail.com>
2025-10-28 15:30:05 -0500

    Request Delete user

--- src/list_users.php
+++ src/list_users.php
@@ -10,10 +10,15 @@ require('../config/database.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Marketapp - List users</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>  
+  
 </head>
 <body>
-    <table border="1" align="center">
-        <tr>
+    <table border="1" class = "table table-striped">
+        <tr class="table-dark">
+              <div class="spinner-border text-info"></div>
+                </div>
        
             <th>Fullname</th>
             <th>E-mail</th>
@@ -25,14 +30,15 @@ require('../config/database.php');
         <?php
         $sql_users = "
         select
-	        u.firstname || '' || u.lastname as fullname,
+            u.id as user_id,
+	        u.firstname || ' ' || u.lastname as fullname,
 	        u.email,
             u. ide_number,
             u.mobile_number,
-	    case 
-            when u.status = true then 'Active' else 'Inactive' end as status
-	    from users u
-     ";
+	        case 
+                when u.status = true then 'Active' else 'Inactive' end as status
+	        from users u
+        ";
         $result = pg_query($conn_local, $sql_users);
         if (!$result){
             die("Error: ".pg_last_error());
@@ -48,7 +54,8 @@ require('../config/database.php');
         <td>
         <a href''#'><img src='icons/search.png' width='20'></a>
         <a href='#'><img src='icons/update.png' width='20'></a>
-        <a href='#'><img src='icons/delete.png' width='20'></a>
+        <a href='delete_users.php?userId=". $row['user_id']."'><img src='icons/delete.png' width='20'></a>
+        <a href='#'><img src='icons/edit.png' width='20'></a>
         </td>
       </tr>";
     }
